dee9a2a9b1b725ab6adaef975c7f0aee39ccd5d8
Mark Adel <mjaaaa1997@gmail.com>
2025-06-29 12:10:29 +0800

    Started Learning PHP

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<!-- NOTES:
+
+$ - In PHP You are declaring a variable using a dollar sign. 
+$string - A pieace of Text.
+$int - A integer or number if you put "" it will become text.
+$float - A number that has decimal points.
+$boolean - true or false statement.
+
+
+
+
+-->
+
+
+<body>
+
+<h3>Calling a Variable</h3>
+
+    <?php
+    $name = "Jianxin";
+    echo $name;
+     ?>
+
+<h3>Datatypes</h3>
+
+    <?php
+    //Scalar Types (Contains One Value)
+    $string = "Mark";
+    $int = 12345;
+    $float = 2.56;
+    $bool = true;
+
+
+     ?>
+
+</body>
+
+</html>
